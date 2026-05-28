93f8d0964575ba4a6dddafb1b707c1ee36037afc
altabanjeh <raedhussam2000@gamil.com>
2026-05-28 14:05:04 +0300

    json fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,24 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+      registrations.forEach((registration) => {
+        registration.unregister().catch(() => null)
+      })
+    })
+
+    if (window.caches?.keys) {
+      window.caches.keys().then((keys) => {
+        keys.forEach((key) => {
+          window.caches.delete(key).catch(() => null)
+        })
+      })
+    }
+  })
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
